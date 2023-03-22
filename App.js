c0f90ae2cef6fd0b3a0c615b8d132a8506e47ac7
bhavya <bhavya.sanamgi@techgrit.com>
2023-03-22 17:08:44 +0530

    Episode-2 about parcel

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const parent = React.createElement(
   'div',
   { id: 'parent' },
   [React.createElement(
     "div",
     { id: 'child' },
-    [React.createElement("h1", {}, "I am at h1 tag"),
+    [React.createElement("h1", {}, "This is react"),
     React.createElement("h2", {}, "I am at h2 tag")]
   ), React.createElement(
     "div",
     { id: 'child2' },
-    [React.createElement("h1", {}, "I am at h1 tag"),
-    React.createElement("h2", {}, "I am at h2 tag")]
+    [React.createElement("h1", {}, "I am at h1 tag")]
   )]
 );
 
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namaste React
+
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR - Hot module replacement
+- File Watching algorithm - written i c++
+- Caching - Faster Builds
+- Image optimization
+- Minification
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code Splitting
+- Differential Bundle - support older browsers
+- Error Handling
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
+-
